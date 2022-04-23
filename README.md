b3bc5ef9c54fc33a914213a6225ec280e35a7809
Chris <cpetrovitch@gmail.com>
2022-04-23 01:05:32 -0400

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# cuSharedContext
+
+ToDO
---
+++ src/common/include/common/oversubTest.hpp
@@ -0,0 +1,43 @@
+/**
+ * @file oversubTest.hpp
+ * @brief Test drivers
+ */
+
+#pragma once
+
+/**
+ * @brief Oversubscription run params
+ */
+struct Params {
+    size_t batchSizeMB = 100; ///< Size (in MB) of each array. This controls the
+                              ///< resolution of the test
+    size_t totalGB = 3; ///< Least mount of memory to allocate. Arrays will be
+                        ///< allocated of size batchSizeMB until the total mount
+                        ///< is larger than totalGB.
+    size_t nIterations = 5; ///< Number of times to run the calculation
+    bool verbose = false;   ///< Print verbose info (per iteration).
+};
+
+/**
+ * @brief Test drivers
+ */
+struct testDriver {
+
+    /**
+     * @brief Driver function for oversubscription test
+     *
+     * This test allocates enough arrays to meet the totalGB desired,
+     * then adds them all together. If you request more memory than
+     * the graphics card has, then it will force the driver to automatically
+     * migrate data to and from the card.
+     *
+     * 0. For each iteration
+     * 1. Allocate N arrays
+     * 2. For each array in arrays: output += array
+     * 3. output = 0 (broadcast)
+     * 4. repeat.
+     *
+     * @param params run parameters
+     */
+    static void oversubscribeTest(Params params);
+};
